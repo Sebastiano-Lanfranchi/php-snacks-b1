5defa283a72d0bbe339ab35f7a69fc09db636e86
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-03-01 14:48:29 +0100

    esercizio corretto con for each

--- index.php
+++ index.php
@@ -6,64 +6,70 @@
   </head>
   <body>
     <?php
-    $calendar=[];
-    $partita1=[
-      "squadraO" => "VL Pesaro",
-      "squadraC" => "Trento",
-      "punteggioO" => "81",
-      "punteggioC" => "70"
-    ];
-    $partita2=[
+    $calendar=[
+      'Partita1'=>[
+        "squadraO" => "VL Pesaro",
+        "squadraC" => "Trento",
+        "punteggioO" => "81",
+        "punteggioC" => "70"
+      ],
+    'partita2'=>[
       "squadraO" => "Brescia",
       "squadraC" => "Venezia",
       "punteggioO" => "71",
       "punteggioC" => "69"
-    ];
-    $partita3=[
+    ],
+    'partita3'=>[
       "squadraO" => "Trieste",
       "squadraC" => "Virtus Bologna",
       "punteggioO" => "60",
       "punteggioC" => "77"
-    ];
-    $partita4=[
+    ],
+    'partita4'=>[
       "squadraO" => "Trento",
       "squadraC" => "Cantu",
       "punteggioO" => "74",
       "punteggioC" => "73"
-    ];
-    $partita5=[
+    ],
+    'partita5'=>[
       "squadraO" => "Olimpia Milano",
       "squadraC" => "Brindisi",
       "punteggioO" => "82",
       "punteggioC" => "88"
-    ];
-    $partita6=[
+    ],
+    'partita6'=>[
       "squadraO" => "Varese",
       "squadraC" => "Reggiana",
       "punteggioO" => "76",
       "punteggioC" => "89"
-    ];
-    $partita7=[
+    ],
+    'partita7'=>[
       "squadraO" => "Universo Treviso",
       "squadraC" => "Cremona",
       "punteggioO" => "101",
       "punteggioC" => "90"
-    ];
-    $partita8=[
+    ],
+    'partita8'=>[
       "squadraO" => "Olimpia Milano",
       "squadraC" => "Cantu",
       "punteggioO" => "55",
       "punteggioC" => "60"
-    ];
-    $partita9=[
+    ],
+    'partita9'=>[
       "squadraO" => "Trieste",
       "squadraC" => "Trento",
       "punteggioO" => "92",
       "punteggioC" => "82"
-    ];
+    ],
+  ];
 
-    array_push( $calendar,$partita1, $partita2, $partita3, $partita4, $partita5, $partita6, $partita7, $partita8, $partita9);
-        var_dump($calendar);
+    foreach ($calendar as $key => $value) {
+      echo $key . ':' . '<br>';
+
+      foreach ($value as $k => $v) {
+        echo $v . '<br>';
+      }
+    }
      ?>
 <br>
      <?php
